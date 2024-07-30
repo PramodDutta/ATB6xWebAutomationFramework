d3099766698ca03c8bf58c25bff569ea29af4b71
Promode <pramoddutta_96@yahoo.com>
2024-07-30 08:24:08 +0530

    ADDING TESTNG EXdasdML

--- src/main/java/com/thetestingacademy/base/CommonToAllPage.java
+++ src/main/java/com/thetestingacademy/base/CommonToAllPage.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.FileNotFoundException;
 import java.time.Duration;
 
-//import static com.thetestingacademy.driver.DriverManager.getDriver;
-import static com.thetestingacademy.driver.DriverManagerSelenoid.getDriver;
+import static com.thetestingacademy.driver.DriverManager.getDriver;
+//import static com.thetestingacademy.driver.DriverManagerSelenoid.getDriver;
 
 public class CommonToAllPage {
 // If you want to call something before every Page Object Class call, Put your Code here")
